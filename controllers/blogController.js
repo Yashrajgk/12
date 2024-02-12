d0677e5adeb7ada1dab8b317cff06b8b369635a3
Yashraj Kanake <111440676+Yashrajgk@users.noreply.github.com>
2024-02-12 17:34:51 +0530

    Update blogController.js

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -204,34 +204,3 @@ exports.commentBlogController = async (req,res) => {
     }
 };
 //DELETE COMMENT
-exports.deleteBlogController = async (req,res) => {
-    const {commentId , blogId, id} = req.params;
-    
-    const userBlog = req.user.userBlog;
-    try{
-        const blog = await blogModel.findById(blogId);
-        if(!blog){
-            res.status(404).json({msg: "Blog not found"});
-        }
-        const commentIndex = blog.comments.findIndex(
-            (comment) => comment._id.toString() === commentId
-        );
-        if (commentIndex === -1) {
-            return res.status(404).json({ msg: "comment not found" });
-        }
-        if (blog.comments[commentIndex].user.toString() != userBlog) {
-            return res.status(403).json({ msg: "Not authorised to delete comment" });
-        }
-        blog.comments.splice(commentIndex,1);
-        await blog.save();
-        res.json({msg: "Deleted successfully"});
-    } catch (error) {
-        console.log(error)
-        return res.status(400).send({
-            success: false,
-            message: 'error in user blog',
-            error,
-        })
-    }
-};
- 
